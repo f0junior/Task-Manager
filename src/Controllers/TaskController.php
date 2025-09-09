65b62f60608d7827c1e9d05aa297ce038e61bea8
Francisco José Machado Junior <39394910+f0junior@users.noreply.github.com>
2025-09-09 14:52:45 +0000

    Primeiro passo criando uma base.

---
+++ src/Controllers/TaskController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+class TaskController
+{
+    public function index(): void
+    {
+        echo "Bem-vindo ao Task Manager!" . PHP_EOL;
+    }
+}
